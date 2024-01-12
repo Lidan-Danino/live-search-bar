5b99a5c1b575d552775e9ab79a443a2c78da0420
lidan <lidandanino@h-MacBook-Pro-sl-lidan-2.local>
2024-01-12 15:41:02 +0200

    create front-end functionality

---
+++ app/components/Countries.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Countries({countries}) {
+  return (
+    <div>  
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,23 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Countries from "../components/Countries";
+
 export default function Home() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountries = async (state) => {
+      const response = await "/api/countries";
+      const countries = await response.json();
+      setCountries(countries);
+    };
+  }, []);
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className="font-bold text-6xl mt-14">Countries Information</h1>
+      <Countries countries={countries} />
     </div>
   );
 }
